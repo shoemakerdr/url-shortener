0dfa5d6f4c5261c04a4baac36be22fc99d779525
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-17 23:12:27 -0500

    Added url shortener module

---
+++ src/shortener.js
@@ -0,0 +1,22 @@
+const db = require('./db')
+
+const isValidURL = url => {
+    // regular expression to for URL testing
+    const rgx = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
+    return rgx.test(url)
+}
+
+const shortener = (resource, response) => {
+    const responses = {
+        error: () => response.json({error: 'URL Invalid'}),
+        json: doc => response.json(doc),
+        redirectTo: url => response.redirect(url)
+    }
+
+    if (isValidURL(resource)) {
+        db.findOrInsertURL(resource, responses)
+    }
+    else db.findShortened(resource, responses)
+}
+
+module.exports = shortener
