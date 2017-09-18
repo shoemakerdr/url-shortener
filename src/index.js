a4e0c91dd4d2c1ca3ed80bcadd7b5a092dd4fb9a
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-17 23:10:50 -0500

    Added URLshortener to /:resource route

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const path = require('path')
 const express = require('express')
-const { timestamp } = require('./timestamp')
+const shortener = require('./shortener')
+
 const app = express()
 
-const indexPath = path.join(__dirname, '../', 'public', 'index.html')
+const publicPath = path.join(__dirname, '../', 'public')
+const html = path.normalize(path.join(publicPath, 'index.html'))
 
-app.use('/', express.static(path.normalize(indexPath)))
+app.use('/', express.static(path.normalize(publicPath)))
 
 app.get('/', (req,res) => {
-    res.sendFile(path.normalize(indexPath))
+    res.sendFile(html)
 })
 
-app.get('/:timestamp', (req, res) => {
-    const ts = req.params.timestamp
-    res.json(timestamp(ts))
+app.get('/:resource', (req, res) => {
+    const resource = req.params.resource
+    shortener(resource, res)
 })
 
 app.listen(process.env.PORT || 8000)
